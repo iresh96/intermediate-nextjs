8141728516d3317f54d3c451afefbb7dab53fcac
Iresh Madushanka <44254469+iresh96@users.noreply.github.com>
2024-11-22 21:51:20 +0100

    chashing ,Loding and Error

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -12,7 +12,6 @@ const Dashboard = ({ children, rsvps, events }) => {
       {path === '/dashboard' ? (
         <div className="flex w-full h-full">
           <div className="w-1/2 border-r border-default-50">{rsvps}</div>
-          <Link href="/dashboard/settings">Events</Link>
           <div className="w-1/2 flex flex-col">
             <div className="border-b border-default-50 w-full h-1/2">
               {events}
---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,7 @@
+import { Spinner } from '@nextui-org/spinner'
+
+const Loader = () => {
+  return <Spinner /> // or anything you want
+}
+
+export default Loader
