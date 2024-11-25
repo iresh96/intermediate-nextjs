10c6abafde8ab448e6222ccd76210809f35153e5
Iresh Madushanka <44254469+iresh96@users.noreply.github.com>
2024-11-25 20:11:51 +0100

    fini-1

---
+++ app/dashboard/guests/page.tsx
@@ -0,0 +1,18 @@
+// /app/dashboard/guests/page.tsx
+import { getGuestList } from '@/utils/attendees'
+import { getCurrentUser } from '@/utils/users'
+
+const GuestsPage = async () => {
+  const user = await getCurrentUser()
+  const guests = await getGuestList(user.id)
+
+  return (
+    <div>
+      {guests.map((guest) => (
+        <div key={guest.id}>{guest.name}</div>
+      ))}
+    </div>
+  )
+}
+
+export default GuestsPage
